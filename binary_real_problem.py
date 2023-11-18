30b03ce7d2b6607f7b35e39dd7cacae58baf01f7
Logeshwaran-N <142582556+Logeshwaran-N@users.noreply.github.com>
2023-11-18 11:55:23 +0530

    Add files via upload

---
+++ binary_real_problem.py
@@ -0,0 +1,36 @@
+a=[5,6,7,8,1,2,3,4]
+b=[5,6,7,8,9,10,11,1,2,3,4]
+c=[8,9,1,2,3,4,5,6,7]
+d=[1,2,3,4]
+e=[1]
+expected=4
+
+def binary_serch(a):
+
+    start=0
+    end=len(a)-1
+    mid = (start+end) // 2
+    if len(a) <= 1 or a[start] < a[end]:
+        return  0
+    while True:
+        # tmid, tsta, tend = a[mid], a[start], a[end]
+        # if a[mid] < a[mid+1] and a[mid] > a[end] and a[mid] > a[start]:
+        #     start=a[mid+1]
+        # else:
+        #     end=mid-1
+
+        mid = (start + end) // 2
+        tmid, tsta, tend = a[mid], a[start], a[end]
+        if mid == start and end == mid+1:
+            return mid+1
+        if a[mid] > a[start]:
+            start=mid
+        else:
+            end=mid
+
+
+print(binary_serch(a))
+print(binary_serch(b))
+print(binary_serch(c))
+print(binary_serch(d))
+print(binary_serch(e))
